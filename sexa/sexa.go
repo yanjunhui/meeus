38575375df15caec0489d25839c256214c5f1171
YanJunhui <i@yanjunhui.com>
2024-08-26 19:50:54 +0800

    调整 sexa

--- sexa/sexa.go
+++ sexa/sexa.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/yanjunhui/unit"
+	"github.com/yanjunhui/meeus/unit"
 )
 
 // Predefined errors indicate that a value could not be formatted.
--- vendor/github.com/soniakeys/sexagesimal/sexa.go
+++ vendor/github.com/soniakeys/sexagesimal/sexa.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/yanjunhui/meeus/unit"
+	"github.com/soniakeys/unit"
 )
 
 // Predefined errors indicate that a value could not be formatted.
